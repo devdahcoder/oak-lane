16b4803a0d149067814508bcf9c1117632ff9522
devdahcoder <adigunolamide200@gmail.com>
2022-09-07 23:14:58 +0100

    feat: added login page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -5,6 +5,7 @@ import Logo from '../../components/Logo';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { VscEyeClosed, VscEye } from 'react-icons/vsc';
+import Link from 'next/link';
 
 
 type Props = {}
@@ -12,6 +13,7 @@ type Props = {}
 const Login = (props: Props) => {
 
     const [hidePassword, setHidePassword] = useState<boolean>(true);
+
     return (
         <div className="flex flex-row items-center justify-center min-h-screen">
             <ContainerSize className={`w-full max-w-md mt-10 flex flex-col space-y-5`}>
@@ -22,64 +24,57 @@ const Login = (props: Props) => {
                         </div>
 
                         <div className="text-2xl font-semibold">
-                            <p>Get Started for free</p>
+                            <p>Log into your account</p>
                         </div>
                     </div>
                     <div className="flex flex-col space-y-5">
-                        <div>
-                            <Input 
-                                labelText={`Full Name`} 
-                                type={`text`} 
-                                className={`py-3.5 px-4 text-base font-medium placeholder:font-extralight`}
-                                containerClassName={` border-gray-400`}
-                                labelClassName={`text-dark-200 font-semibold text-base tracking-wider`}
-                                placeholder={`Annie Helsinki`}
-                                mainContainerClassName={`flex flex-col space-y-1.5`}
-                            />
-                        </div>
-                        <div>
-                            <Input 
-                                labelText={`Work Email Address`} 
-                                type={`text`} 
-                                className={`py-3.5 px-4 text-base font-medium placeholder:font-extralight`}
-                                containerClassName={` border-gray-400`}
-                                labelClassName={`text-dark-200 font-semibold text-base tracking-wider`}
-                                placeholder={`Anniehel007@gmail.com`}
-                                mainContainerClassName={`flex flex-col space-y-1`}
-                            />
-                        </div>
-                        <div>
-                            <Input 
-                                labelText={`Password`} 
-                                type={`${hidePassword ? 'password' : 'text'}`} 
-                                className={`py-3.5 px-4 text-base font-medium placeholder:font-extralight`}
-                                containerClassName={` border-gray-400`}
-                                labelClassName={`text-dark-200 font-semibold text-base tracking-wider`}
-                                placeholder={`Enter Your Password`}
-                                mainContainerClassName={`flex flex-col space-y-1`}
-                                rightIcon={!hidePassword ? <VscEyeClosed className='w-5 h-5' /> : <VscEye className='w-5 h-5' />}
-                                isRightIcon={true}
-                                rightIconContainerClassName={`right-2 border`}
-                                rightIconClick={() => {setHidePassword(!hidePassword)}}
-                            />
+                        <div className="flex flex-col space-y-1">
+                            <div className="flex flex-col space-y-5">
+                                <div>
+                                    <Input 
+                                        labelText={`Work Email Address`} 
+                                        type={`text`} 
+                                        className={`py-3.5 px-4 text-base font-medium placeholder:font-extralight`}
+                                        containerClassName={` border-gray-400`}
+                                        labelClassName={`text-dark-200 font-semibold text-base tracking-wider`}
+                                        placeholder={`Anniehel007@gmail.com`}
+                                        mainContainerClassName={`flex flex-col space-y-1`}
+                                    />
+                                </div>
+                                <div>
+                                    <Input 
+                                        labelText={`Password`} 
+                                        type={`${hidePassword ? 'password' : 'text'}`} 
+                                        className={`py-3.5 px-4 text-base font-medium placeholder:font-extralight`}
+                                        containerClassName={` border-gray-400`}
+                                        labelClassName={`text-dark-200 font-semibold text-base tracking-wider`}
+                                        placeholder={`Enter Your Password`}
+                                        mainContainerClassName={`flex flex-col space-y-1`}
+                                        rightIcon={!hidePassword ? <VscEyeClosed className='w-5 h-5' /> : <VscEye className='w-5 h-5' />}
+                                        isRightIcon={true}
+                                        rightIconContainerClassName={`right-2 border`}
+                                        rightIconClick={() => {setHidePassword(!hidePassword)}}
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-row items-center justify-end">
+                                <a className="text-blue-300 " href="http://" target="_blank" rel="noopener noreferrer">Forgotten Password?</a>
+                            </div>
                         </div>
                         <div>
                             <Button 
                                 className={`w-full bg-green-100 text-center flex flex-row items-center justify-center rounded-sm py-4
                                 text-white font-medium hover:bg-green-400 transition-all duration-300 ease-linear`} 
-                                text={`Get Started`} 
+                                text={`Log in`} 
                             />
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-col items-center justify-center text-center space-y-5">
                     <div>
-                        <a className="text-blue-300 " href="http://" target="_blank" rel="noopener noreferrer">I have an account</a>
-                    </div>
-                    <div>
-                        <div className="text-dark-200">Signing up for OakLane means you agree to the <a className="text-blue-300 " href="http://" target="_blank" rel="noopener noreferrer">
-                            Privacy Policy</a> and</div>
-                        <a className="underline text-blue-300 " href="http://" target="_blank" rel="noopener noreferrer">Terms of Service</a>
+                        <Link href={`/auth/sign-up`}>
+                            <a className="text-blue-300 " href="" >Create an account</a>
+                        </Link>
                     </div>
                 </div>
             </ContainerSize>
